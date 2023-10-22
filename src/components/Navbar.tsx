04961e6f45db4f7bbccc72d32ebe97ebb465e529
CrypticSignal <hshafiq@hotmail.co.uk>
2023-10-22 10:56:58 +0100

    Remove Next.js components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,11 @@
-import Image from "next/image";
 import gitHubLogo from "../images/GitHub-Mark-Light-32px.png";
-import Link from "next/link";
 
 const Navbar = () => {
-  const setClassName = (event: React.MouseEvent<HTMLElement>) => {
-    event.preventDefault();
-    let linksDiv = document.getElementById("links")!;
-    if (linksDiv.className === "top") {
-      linksDiv.className += " mobile";
-    } else {
-      linksDiv.className = "top";
-    }
-  };
-
   return (
     <div id="links" className="top">
-      <Link href="/" className="hamburger" onClick={setClassName}>
-        <i className="fa fa-bars"></i>
-      </Link>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/filetypes">Filetypes</Link>
-      <Link href="/yt">YT downloader</Link>
-      <Link
-        href="/game"
-        onClick={() => {
-          window.location.href = "game";
-        }}
-      >
-        Game
-      </Link>
-      <Link href="https://github.com/CrypticSignal/av-converter" id="github_link">
-        <Image src={gitHubLogo} alt="github logo" />
-      </Link>
+      <a href="https://github.com/CrypticSignal/av-converter" id="github_link">
+        <img src={gitHubLogo} alt="github logo" />
+      </a>
     </div>
   );
 };
